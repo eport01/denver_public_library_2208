37c3f1c72f6bf4b2f4843b2963a0d3f35316281d
Emily Port <emilylport@gmail.com>
2022-09-14 09:46:12 -0600

    Add an author can write books

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,17 @@
+require './book'
 class Author 
+  attr_reader :books, :name, :write
   def initialize(author_details)
     @author_details = author_details
+    @books = []
   end
 
   def name 
     @author_details[:first_name] + " " + @author_details[:last_name]
+  end
+
+  def write(title, publication_date)
+    book = Book.new({title: title, publication_date: publication_date})
+
   end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -11,4 +11,19 @@ RSpec.describe Author do
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
     expect(charlotte_bronte.name).to eq("Charlotte Bronte")
   end
+
+  it 'has books' do 
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    expect(charlotte_bronte.books).to eq([])
+  end
+
+  it 'can write books' do 
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    require 'pry'; binding.pry
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    expect(jane_eyre.class).to eq(Book)
+    expect(jane_eyre.title).to eq("Jane Eyre")
+    villette = charlotte_bronte.write("Villette", "1853")
+    expect(charlotte_bronte.books).to eq(jane_eyre, villette)
+  end
 end
