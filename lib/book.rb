c6d25899b96af00a116b8f34ae891a05212dcec4
Emily Port <emilylport@gmail.com>
2022-09-14 09:08:35 -0600

    Add author

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,14 @@
 class Book
+  attr_reader :title, :book_info, :author
   def initialize(book_info)
-    @book_info
+    @book_info = book_info
+  end
+
+  def title 
+    @book_info[:title]
+  end
+
+  def author
+    @book_info[:author_first_name] + " " + @book_info[:author_last_name]
   end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Book do
     book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
     expect(book).to be_an_instance_of(Book)
   end
+
+  it 'has a title and author' do 
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    expect(book.title).to eq("To Kill a Mockingbird")
+    expect(book.author).to eq("Harper Lee")
+  end
+
+  
 end
