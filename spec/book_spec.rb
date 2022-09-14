63ce7bd4ef32854909057ebff662d622cec2399f
Emily Port <emilylport@gmail.com>
2022-09-14 09:12:16 -0600

    add publication year

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -13,5 +13,11 @@ RSpec.describe Book do
     expect(book.author).to eq("Harper Lee")
   end
 
+  it 'has a publication year' do 
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    expect(book.publication_year).to eq("1960")
+
+  end
+
   
 end
